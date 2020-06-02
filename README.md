b8c181950bc8d4c6d10207720e4674624a3a1dd7
chungen1007 <en1007en@gmail.com>
2020-06-02 14:22:25 +0800

    Add main.cpp and modify README.md
    add main() in main.cpp and put class definition in README.md.

--- README.md
+++ README.md
@@ -11,3 +11,16 @@ main.cpp
     |-- data_structure2.cpp
     |-- data_structure3.cpp
 ```
+
+## Main Class
+```
+class MailManager {
+public:
+  //need to provide a default constructor and 4 member functions
+  MailManager();
+  void add(std::string path_to_file);
+  void remove(int id);
+  void longest();
+  void query(Query q);
+};
+```
--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
+#include <cstring>
+#include <string>
 #include <iostream>
+#include "common.hpp"
+#include "mail_manager.hpp"
 
-using namespace std;
+MailManager mailbox;
 
-int main(){
-	cout << "hi2" << endl;
-	return 0;
+int main(void)
+{
+  char command[8];
+  std::string file_path;
+  int id;
+  Query q;
+
+  while (std::cin >> command) {
+    if (strcmp(command, "add")) {
+      cin >> file_path;
+      mailbox.add(file_path);
+    } else if (strcmp(command, "remove")) {
+      cin >> id;
+      mailbox.remove(id);
+    } else if (strcmp(command, "longest")) {
+      mailbox.longest();
+    } else if (strcmp(command, "query")) {
+      cin >> q;
+      mailbox.query(q);
+    }
+  }
+  return 0;
 }
